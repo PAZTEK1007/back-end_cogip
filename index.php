3103f0cb89520efe1eeb63eb69a564b44b5ea5de
Mathias Barbier <mathbarbier07@gmail.com>
2023-12-05 15:18:56 +0100

    del files

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-//charger les variabales d'envirronement à partir du fichier .env dans le répertoire courant
-Dotenv\Dotenv::createUnsafeImmutable(__DIR__)->load();
-require_once __DIR__ . '/Core/Helper.php';
-require_once __DIR__ . '/Routes/Routes.php';

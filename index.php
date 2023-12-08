1785587a63e5b088b2bfbaf2b26fd1f91d3b1232
Mathias <mathbarbier07@gmail.com>
2023-12-08 15:23:51 +0100

    del master

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// var_dump($_ENV);
-
-require_once __DIR__ . '/vendor/autoload.php';
-//charger les variabales d'envirronement à partir du fichier .env dans le répertoire courant
-Dotenv\Dotenv::createUnsafeImmutable(__DIR__)->load();
-require_once __DIR__ . '/Core/Helper.php';
-require_once __DIR__ . '/Routes/Routes.php';

c8395720fb12ccf5b7e16e9e4aa88a083422f9e5
Mathias Barbier <mathbarbier07@gmail.com>
2023-12-12 11:35:46 +0100

    update

--- Routes/Routes.php
+++ Routes/Routes.php
@@ -7,6 +7,7 @@ use App\Controllers\HomeController;
 
 
 $router = new Router();
+
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
     // you want to allow, and if so:
@@ -14,11 +15,6 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
 }
-$router->before('GET|POST|PUT|DELETE', '/.*', function () {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type');
-});
 
 $router->mount('/api', function () use ($router) {
     // GET METHOD  //////////////////////////////////////////////////////
